dd3234eae0b3a5fe0f8f417712645f7f7236237a
michael tu <michael.tu@yale.edu>
2023-02-17 15:20:45 -0500

    added xori

--- cpsc420-lab2/pv2stall/pv2stall-CoreCtrl.v
+++ cpsc420-lab2/pv2stall/pv2stall-CoreCtrl.v
@@ -367,6 +367,7 @@ module parc_CoreCtrl
 
       // My Instructions
       `PARC_INST_MSG_ANDI    :cs={ y,  n,    br_none, pm_p,   am_rdat, y, bm_zi,   n, alu_and,  md_x,    n, mdm_x, em_alu, nr,  ml_x, dmm_x,  wm_alu, y,  rt, n   };
+      `PARC_INST_MSG_XORI    :cs={ y,  n,    br_none, pm_p,   am_rdat, y, bm_zi,   n, alu_xor,  md_x,    n, mdm_x, em_alu, nr,  ml_x, dmm_x,  wm_alu, y,  rt, n   };
 
     endcase
 
